50f54c90cea629a870549186493f7419fa34d542
Alexandre <mybrainisout@gmail.com>
2015-04-21 16:47:05 +0200

    Niveau
    Debut des pages niveau

--- KEEPIN_Java/src/net/keepin/ui/ModifNiveau.java
+++ KEEPIN_Java/src/net/keepin/ui/ModifNiveau.java
@@ -18,9 +18,6 @@ public class ModifNiveau{
 		lblService.setBounds(350, 360, 160, 25);
 		modifNiveau.getContentPane().add(lblService);
 
-		JLabel lblSalle = new JLabel("Selctionner Salle");
-		lblSalle.setBounds(350, 410, 160, 25);
-		modifNiveau.getContentPane().add(lblSalle);
 
 		Bouton boutonAnnuler = new Bouton ("Annuler", 350, 128, 0);
 		modifNiveau.getContentPane().add(boutonAnnuler);
@@ -28,38 +25,15 @@ public class ModifNiveau{
 		Bouton boutonAjouter = new Bouton ("Modifier", 630, 0, 128);
 		modifNiveau.getContentPane().add(boutonAjouter);
 		
-		JComboBox cbxNiveau = new JComboBox();
+		final ComboNiveau cbxNiveau = new ComboNiveau();
 		cbxNiveau.setBounds(500, 310, 160, 25);
 		modifNiveau.getContentPane().add(cbxNiveau);
 		
-		JComboBox cbxService = new JComboBox();
+		final ComboService cbxService = new ComboService();
 		cbxService.setBounds(500, 360, 160, 25);
 		modifNiveau.getContentPane().add(cbxService);
 		
-		JComboBox cbxBatiment = new JComboBox();
-		cbxBatiment.setToolTipText("");
-		cbxBatiment.setBounds(500, 410, 100, 25);
-		modifNiveau.getContentPane().add(cbxBatiment);
 		
-		JComboBox cbxEtage = new JComboBox();
-		cbxEtage.setToolTipText("");
-		cbxEtage.setBounds(625, 410, 75, 25);
-		modifNiveau.getContentPane().add(cbxEtage);
-		
-		JComboBox cbxSalle = new JComboBox();
-		cbxSalle.setToolTipText("");
-		cbxSalle.setBounds(725, 410, 100, 25);
-		modifNiveau.getContentPane().add(cbxSalle);
-		
-		JButton btnTick = new JButton("");
-		btnTick.setIcon(new ImageIcon("C:\\wamp\\www\\KeepIn\\Web\\SiteKEEPIN\\image\\check.png"));
-		btnTick.setBounds(850, 410, 25, 25);
-		modifNiveau.getContentPane().add(btnTick);
-		
-		JButton btnMark = new JButton("");
-		btnMark.setIcon(new ImageIcon("C:\\wamp\\www\\KeepIn\\Web\\SiteKEEPIN\\image\\mark.png"));
-		btnMark.setBounds(900, 410, 25, 25);
-		modifNiveau.getContentPane().add(btnMark);
 
 		modifNiveau.setVisible(true);
 
